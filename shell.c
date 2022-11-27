c9f0af7592502b3c88f0091e134972a8b7f26cb4
AlanGacihi <alanmamicha@gmail.com>
2022-11-27 13:08:30 +0300

    first commit

--- shell.c
+++ shell.c
@@ -209,7 +209,7 @@ int find_fullpath( command_t* p_cmd ) {
     struct stat buff;
     int found = 0;
 
-    printf("%s\n", path_env_variable);
+    printf("%s\n", getenv( "PATH" ));
     for (char *p = strtok(path_env_variable,":"); p != NULL; p = strtok(NULL, ":")) {
         printf("%s\n", p);
     }
